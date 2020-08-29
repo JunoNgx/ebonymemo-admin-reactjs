1a3e7b728815b83764d3f3a9ddd2c811129038e3
JunoNgx <juno.ngx@gmail.com>
2020-08-29 13:08:25 +0800

    Minor content update

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <div className="header">
             <h1>EbonyMemo</h1>
-            <h2>Admin Panel</h2>
+            <h2>Content management panel</h2>
         </div>
     )
 }
